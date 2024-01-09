cf1579a9093b4effb96b16120209080c279c5610
Hooke <a.huk@platma.com>
2024-01-09 16:48:11 +0200

    ver 1.0.2 - added a change option

--- platma-inbuilddb.js
+++ platma-inbuilddb.js
@@ -1,3 +1,4 @@
+const axios = require("axios");
 module.exports = function (RED) {
   const axios = require('axios');
   const CORESERVICE_API_HOST = process?.env?.CORESERVICE_API_HOST;
@@ -5,17 +6,17 @@ module.exports = function (RED) {
   const userId = parseInt(process?.env?.USER_ID);
   const appId = parseInt(process?.env?.APP_ID);
 
-  if (!CORESERVICE_API_HOST || !token || !userId || !appId){
-    node.error(RED._('platma-inbuilddb.errors.lack-envs'));
-    node.status({ fill: 'red', shape: 'dot', text: 'Error. There is a lack of env data' });
-    nodeDone();
-    return;
-  }
-
   function PlatmaInbuildDb(config) {
     RED.nodes.createNode(this, config);
     const node = this;
     node.on('input', function (msg, nodeSend, nodeDone) {
+
+      if (!CORESERVICE_API_HOST || !token || !userId || !appId){
+        node.error(RED._('platma-inbuilddb.errors.lack-envs'));
+        node.status({ fill: 'red', shape: 'dot', text: 'Error. There is a lack of env data' });
+        nodeDone();
+        return;
+      }
 
       node.status({
         fill: 'green',
@@ -30,19 +31,33 @@ module.exports = function (RED) {
         return;
       }
 
+      let byTableId;
+      if ((config.method === 'getone' || config.method === 'change') && !msg.tableId){
+        node.error(RED._('platma-inbuilddb.errors.no-tableId'));
+        node.status({ fill: 'red', shape: 'dot', text: 'Error. No tableId' });
+        nodeDone();
+        return;
+      } else {
+        byTableId = `?id=eq.${msg?.tableId}`
+      }
+
+      if ((config.method === 'getone' || config.method === 'change') && !msg.tableItem){
+        node.error(RED._('platma-inbuilddb.errors.no-tableItem'));
+        node.status({ fill: 'red', shape: 'dot', text: 'Error. No no-tableItem' });
+        nodeDone();
+        return;
+      }
+
       node.status({
         fill: 'blue',
         shape: 'dot',
         text: 'platma-inbuilddbp.status.requesting',
       });
 
-      let getByTableId;
-      if (config.method === 'getone') getByTableId = `?id=eq.${msg?.tableId || 1}`
-
       if (config.method === 'getall' || config.method === 'getone') {
         axios({
           method: 'get',
-          url: `${CORESERVICE_API_HOST}/tooljet_db/organizations/node-red/$%7B${config.tablename}%7D${getByTableId}`,
+          url: `${CORESERVICE_API_HOST}/tooljet_db/organizations/node-red/$%7B${config.tablename}%7D${byTableId || ''}`,
           headers: {
             Authorization: `Bearer ${token}`,
             userId,
@@ -50,44 +65,12 @@ module.exports = function (RED) {
           },
         })
             .then((res) => {
-              msg.statusCode = res.status;
-              const body = res.data;
-              msg.payload = {
-                success: body.success,
-                rawResponse: body,
-              };
-              node.status({});
-              nodeSend(msg);
-              nodeDone();
+              setResponse(msg, res, node, nodeSend, nodeDone);
             })
             .catch((err) => {
-              if (err.code === 'ETIMEDOUT' || err.code === 'ESOCKETTIMEDOUT') {
-                node.error(RED._('common.notification.errors.no-response'), msg);
-                node.status({
-                  fill: 'red',
-                  shape: 'ring',
-                  text: 'common.notification.errors.no-response',
-                });
-              } else {
-                node.error(err, msg);
-                node.status({fill: 'red', shape: 'ring', text: err.code});
-              }
-              msg.payload = err.toString();
-              msg.statusCode =
-                  err.code || (err.response ? err.response.statusCode : undefined);
-
-              if (!config.senderr) {
-                nodeSend(msg);
-              }
-              nodeDone();
+              catchError(err, node, msg, config, nodeSend, nodeDone)
             });
       }  else if (config.method === 'store'){
-        if (!msg.tableItem){
-          node.error(RED._('platma-inbuilddb.errors.no-tableItem'));
-          node.status({ fill: 'red', shape: 'dot', text: 'Error. No no-tableItem' });
-          nodeDone();
-          return;
-        }
         axios({
           method: 'post',
           url: `${CORESERVICE_API_HOST}/tooljet_db/organizations/node-red/$%7B${config.tablename}%7D`,
@@ -99,36 +82,27 @@ module.exports = function (RED) {
           data: {...msg.tableItem}
         })
             .then((res) => {
-              msg.statusCode = res.status;
-              const body = res.data;
-              msg.payload = {
-                success: body.success,
-                rawResponse: body,
-              };
-              node.status({});
-              nodeSend(msg);
-              nodeDone();
+              setResponse(msg, res, node, nodeSend, nodeDone);
             })
             .catch((err) => {
-              if (err.code === 'ETIMEDOUT' || err.code === 'ESOCKETTIMEDOUT') {
-                node.error(RED._('common.notification.errors.no-response'), msg);
-                node.status({
-                  fill: 'red',
-                  shape: 'ring',
-                  text: 'common.notification.errors.no-response',
-                });
-              } else {
-                node.error(err, msg);
-                node.status({fill: 'red', shape: 'ring', text: err.code});
-              }
-              msg.payload = err.toString();
-              msg.statusCode =
-                  err.code || (err.response ? err.response.statusCode : undefined);
-
-              if (!config.senderr) {
-                nodeSend(msg);
-              }
-              nodeDone();
+              catchError(err, node, msg, config, nodeSend, nodeDone)
+            });
+      } else if (config.method === 'change'){
+        axios({
+          method: 'put',
+          url: `${CORESERVICE_API_HOST}/tooljet_db/organizations/node-red/$%7B${config.tablename}%7D${byTableId}`,
+          headers: {
+            Authorization: `Bearer ${token}`,
+            userId,
+            appId
+          },
+          data: {...msg.tableItem}
+        })
+            .then((res) => {
+              setResponse(msg, res, node, nodeSend, nodeDone);
+            })
+            .catch((err) => {
+              catchError(err, node, msg, config, nodeSend, nodeDone);
             });
       }
     })
@@ -144,3 +118,37 @@ module.exports = function (RED) {
   }
   RED.nodes.registerType('platma-inbuilddb', PlatmaInbuildDb);
 };
+
+function setResponse(msg, res, node, nodeSend, nodeDone) {
+  msg.statusCode = res.status;
+  const body = res.data;
+  msg.payload = {
+    success: body.success,
+    rawResponse: body,
+  };
+  node.status({});
+  nodeSend(msg);
+  nodeDone();
+}
+
+function catchError(err, node, msg, config, nodeSend, nodeDone) {
+  if (err.code === 'ETIMEDOUT' || err.code === 'ESOCKETTIMEDOUT') {
+    node.error(RED._('common.notification.errors.no-response'), msg);
+    node.status({
+      fill: 'red',
+      shape: 'ring',
+      text: 'common.notification.errors.no-response',
+    });
+  } else {
+    node.error(err, msg);
+    node.status({fill: 'red', shape: 'ring', text: err.code});
+  }
+  msg.payload = err.toString();
+  msg.statusCode =
+      err.code || (err.response ? err.response.statusCode : undefined);
+
+  if (!config.senderr) {
+    nodeSend(msg);
+  }
+  nodeDone();
+}
